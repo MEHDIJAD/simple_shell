43e5072277fb5dddaca0cb304679068a897cecea
El Mehdi El Garouaz <eel-garo@e2r4p14.1337.ma>
2025-09-20 09:33:06 +0100

    Betty Task101

--- Task101.c
+++ Task101.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point for the simple shell program
+ *
+ * Description: This is a basic shell program that prints a message.
+ * This serves as the foundation for building a UNIX command interpreter.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("This is My second shell\n");
-
 	return (0);
 }
